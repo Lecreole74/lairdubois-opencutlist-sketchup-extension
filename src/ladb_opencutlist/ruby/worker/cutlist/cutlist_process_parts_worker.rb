5a4a54f006e21e4927c54bb7e0ba163aaeb22698
Lecreole74 <lecreole74@gmail.com>
2025-12-15 01:45:17 +0100

    reinstallation

--- src/ladb_opencutlist/ruby/worker/cutlist/cutlist_process_parts_worker.rb
+++ src/ladb_opencutlist/ruby/worker/cutlist/cutlist_process_parts_worker.rb
@@ -31,7 +31,7 @@ module Ladb::OpenCutList
       return { :errors => [ 'tab.cutlist.error.no_model' ] } unless model
 
       # Retrieve part
-      parts = @cutlist.get_real_parts(@part_ids)
+      parts = @cutlist.get_parts(@part_ids)
       return { :errors => [ 'tab.cutlist.error.unknow_part' ] } if parts.empty?
       # Ask for output dir
       dir = UI.select_directory(title: PLUGIN.get_i18n_string('tab.cutlist.write.title'), directory: '')
